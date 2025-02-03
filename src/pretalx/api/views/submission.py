341a482d8c32a14aed7fe57372bb9c8041a52d88
Tobias Kunze <r@rixx.de>
2025-02-03 15:49:49 +0100

    Add explicit ordering to paginated API
    closes #1948

--- src/pretalx/api/views/submission.py
+++ src/pretalx/api/views/submission.py
@@ -42,7 +42,7 @@ class SubmissionViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         base_qs = self.request.event.submissions.all().annotate(
             favourite_count=Count("favourites")
-        )
+        ).order_by("code")
         if self.request._request.path.endswith(
             "/talks/"
         ) or not self.request.user.has_perm(
